bbdeff16b62a3c5e148b2db1b73cbbc5bf6e09f0
zefrics <zefrics@naver.com>
2024-08-30 01:54:44 +0900

    0.0.3
    1. 전반적인 위계 작업
    2. 2번계열 페이지 완료
    
    다음에 할 일
    1. 메뉴에 아이콘 넣기
    2. 2번 계열 완성하기 (이미지 제외)
    3. 3번 계열 시작하기

--- js/menu-activate.js
+++ js/menu-activate.js
@@ -1,12 +1,21 @@
-// 만약 url에 home이 있으면 -> id="p-home"의 클래스에 active추가
+// 만약 url에 index이 있으면 -> id="p-index"의 클래스에 active추가
 // 만약 url에 2-1이 있으면 -> id="p-2","p-2-1"의 클래스에 active추가
 
-var current = window.location.pathname //ex. "/2-1.html"
-var extract1 = current.replace("/","").replace(".html","") //ex. "2-1"
-var extract2 = extract1.charAt(0) // 첫글자만 가져오기 ex. "2"
+var current = window.location.href //ex. "http://www.naver.com/2-2-1.html"
+var lastItem = current.substring(current.lastIndexOf('/') + 1) //ex. 2-2-1.html 
 
-document.getElementById("p-"+extract1).classList.add('active')
+var extract1 = lastItem.replace("/","").replace(".html","") //ex. "2-2-1"
+var extract2 = extract1.substring(0, 3) // 첫 세글자만 가져오기 ex. "2-2"
+var extract3 = extract1.substring(0, 1) // 첫 글자만 가져오기 ex. "2"
+
+if (document.getElementById("p-"+extract1)) {
+  document.getElementById("p-"+extract1).classList.add('active')
+} // 2-2-1 페이지 접속 시, 2-2-1가 있으면 켜준다.
 
 if (document.getElementById("p-"+extract2)) {
   document.getElementById("p-"+extract2).classList.add('active')
-} // 만약 해당 ID가 존재한다면 (상위 타이틀이 있다면), 이것도 활성화
+} // 2-2-1 페이지 접속 시, 2-2가 있으면 켜준다.
+
+if (document.getElementById("p-"+extract3)) {
+  document.getElementById("p-"+extract3).classList.add('active')
+} // 2-2-1 페이지 접속 시, 2가 있으면 켜준다.
