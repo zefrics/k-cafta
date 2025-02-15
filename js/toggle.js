0a194d625d2e08dd8bc412b32e8a1a6c0e6bd024
zefrics <zefrics@naver.com>
2025-02-15 22:43:57 +0900

    1.6.3
    1. Success Stories 내용 변경
    2. 메뉴 구조 변경중

--- js/toggle.js
+++
@@ -1,8 +0,0 @@
-function myToggle() {
-  var target = document.getElementById("my-menu");
-  target.classList.toggle('hide');
-}
-
-function toTop() {
-  window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
-}
